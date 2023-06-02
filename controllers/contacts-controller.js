663b254b77f616e93c083df9a8de775a4942abfa
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-06-03 00:16:49 +0300

    done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -5,7 +5,13 @@ const { HttpError } = require("../helpers");
 const { controllerWrapper } = require("../decorators");
 
 const getContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip: 2,
+    limit: 2,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
@@ -19,7 +25,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
